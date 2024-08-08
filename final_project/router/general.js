74fdeb15ede1b7663300dab82d0b1d51a4ba59f1
xinqiguo1 <xinqiguo@umich.edu>
2024-08-08 19:04:48 -0400

    updated

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -7,25 +7,36 @@ const public_users = express.Router();
 
 public_users.post("/register", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books,null,4));
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  res.send(res.status(200).json(books[isbn]));
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // get array of matching book objects
+  const author = req.params.author
+  let matchingBooks = books.filter(
+    (book) => book.author.toLowerCase() === author.toLowerCase()
+  );
+  res.send(matchingBooks);
+//   if (matchingBooks.length > 0) {
+//     return res.status(200).send(JSON.stringify(matchingBooks, null, 4));
+//   } else {
+//     return res.status(404).json({ message: "No books by that author." });
+//   }
 });
 
 // Get all books based on title
